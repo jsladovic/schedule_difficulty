7f9cfe6c851c225a9b8bfe8f361b44798a004ae1
jsladovi <janko.sladovic@gmail.com>
2018-04-25 18:43:25 +0200

    added schedule difficulty calculation

--- analyzer.py
+++ analyzer.py
@@ -1,12 +1,15 @@
 
 from __future__ import division
 from datetime import datetime
+import operator
 
 class Analyzer:
     def __init__(self, matches, competition):
         self.matches_by_team = matches
         self.competition = competition
         self.point_averages = self.calculate_average_points()
+        self.home_point_averages = self.calculate_average_points(True)
+        self.away_point_averages = self.calculate_average_points(False)
 
     def matches_in_competition(self, team):
         return [match for match in self.matches_by_team[team] if match.tournament == self.competition]
@@ -15,25 +18,54 @@ class Analyzer:
         self.calculate_expected_points_for_matches()
         self.calculte_average_points_around_matches(2, 2, True)
         for team in self.matches_by_team:
-            print '\n' + team
-            sum_avg = 0
-            for match in self.matches_by_team[team]:
-                if match.tournament != self.competition:
-                    continue
-                print str(match) + ' -> ' + str(round(match.points_average_around_match, 2))
-                sum_avg += match.points_average_around_match
-            print sum_avg
+            matches = sorted(self.matches_in_competition(team), key = lambda x: x.points_average_around_match)
 
-    def calculate_average_points(self):
+            biggest_diffs = self.find_biggest_differences(matches)
+            difficulty = 0
+            for i in range(0, len(matches)):
+                if difficulty < 4 and i > biggest_diffs[difficulty][0]:
+                    difficulty += 1
+                #matches[i].difficulty = difficulty + 1
+                matches[i].difficulty = int(5 * (i / len(matches)) + 1)
+
+        difficulties = []
+        for team in self.matches_by_team:
+            matches = self.matches_in_competition(team)
+            difficulty_sum = 0
+            for match in matches:
+                opposition_match = [m for m in  self.matches_in_competition(match.other_team(team)) if m == match][0]
+                difficulty_sum += opposition_match.difficulty
+            difficulties.append((team, difficulty_sum / len(matches)))
+
+        difficulties = self.sort(difficulties, True)
+        for i in range(0, len(difficulties)):
+            print str(i + 1) + '. ' + difficulties[i][0] + '\t\t' + str(round(difficulties[i][1], 2))
+
+    def calculate_average_points(self, home = None):
         teams = {}
         for team in self.matches_by_team:
             matches_in_competition = self.matches_in_competition(team)
             points_sum = 0
+            count = 0
             for match in matches_in_competition:
-                points_sum += match.points_for_team(team)
+                if home == None:
+                    points_sum += match.points_for_team(team)
+                    count += 1
+                elif match.is_home_team(team) == home:
+                    points_sum += match.points_for_team(team)
+                    count += 1
 
-            teams[team] = points_sum/len(matches_in_competition)
+            teams[team] = points_sum/count
+            
         return teams
+
+    def find_biggest_differences(self, matches):
+        differences = dict()
+        for i in range(0, len(matches) - 1):
+            differences[i] = matches[i + 1].points_average_around_match - matches[i].points_average_around_match
+
+        biggest_diffs = sorted(differences.items(), key = operator.itemgetter(1), reverse = True)[:4]
+        return sorted(biggest_diffs, key = lambda x : x[0])
 
     def calculte_average_points_around_matches(self, past_matches, future_matches, relative = False):
         for team in self.matches_by_team:
@@ -57,14 +89,29 @@ class Analyzer:
     def calculate_expected_points_for_matches(self):
         for team in self.matches_by_team:
             for match in self.matches_in_competition(team):
-                match.expected_points = self.calculate_expected_points_for_match(match, team)
+                match.expected_points = self.calculate_expected_points_for_match_home_away(match, team)
 
     def calculate_expected_points_for_match(self, match, team):
         team_average_points = self.point_averages[team]
         other_team_average_points = self.point_averages[match.other_team(team)]
         if match.is_home_team(team):
             return 3 * 2 * team_average_points / (2 * team_average_points + other_team_average_points)
+
         return 3 * team_average_points / (team_average_points + 2 * other_team_average_points)
+
+    def calculate_expected_points_for_match_home_away(self, match, team):
+        if match.is_home_team(team):
+            team_average_points = self.home_point_averages[team]
+        else:
+            team_average_points = self.away_point_averages[team]
+
+        other_team = match.other_team(team)
+        if match.is_home_team(other_team):
+            other_team_average_points = self.home_point_averages[other_team]
+        else:
+            other_team_average_points = self.away_point_averages[other_team]
+
+        return 3 * team_average_points / (team_average_points + other_team_average_points)
 
     def calculate_time_between_matches(self):
         teams_rest_difference = []
@@ -110,5 +157,5 @@ class Analyzer:
 
             return (match.date.date() - self.matches_by_team[team][i - 1].date.date()).days - 1
             
-    def sort(self, array):
-        return sorted(array, key = lambda x: x[1])
+    def sort(self, array, rev = False):
+        return sorted(array, key = lambda x: x[1], reverse = rev)
